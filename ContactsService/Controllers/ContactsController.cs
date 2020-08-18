e0a63458dc5f813899750dd6be661a6fe96c4221
Nell <nbrisard@coxautoinc.com>
2020-08-17 22:42:26 -0500

    adding method to SaltPassword

--- ContactsService/Controllers/ContactsController.cs
+++ ContactsService/Controllers/ContactsController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ContactsService.Models;
 using System.Collections;
+using System.Security.Cryptography;
 
 namespace ContactsService.Controllers
 {
@@ -38,12 +39,15 @@ namespace ContactsService.Controllers
             //TODO wrap in try /catch
             value.Id = currentId;
             value.DateAdded = value.DateModified = DateTime.Now;
+
+            value.Password = saltPassword(value.Password);
+
             contacts.Add(value);
             currentId++;
         }
 
-        // PUT: api/Contacts/5
-        [HttpPut("{id}")]
+		// PUT: api/Contacts/5
+		[HttpPut("{id}")]
         public void Put(int id, [FromBody] Contact value)
         {
             var temp = contacts.FirstOrDefault(x => x.Id == id);
@@ -61,6 +65,19 @@ namespace ContactsService.Controllers
         {
             var temp = contacts.FirstOrDefault(x => x.Id == id);
             temp = null;
+        }
+
+
+        private string saltPassword(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+            return Convert.ToBase64String(hashBytes);
         }
     }
 }
