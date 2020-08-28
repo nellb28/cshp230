6246cbf3ed78a18fa56dad97a27f1bebc133a10e
Nell <nbrisard@coxautoinc.com>
2020-08-28 11:54:09 -0500

    adding missing file

---
+++ Project1/Models/UserModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelloWorld.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
