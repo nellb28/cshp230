f1fb2729fc6c623d4dbeedc412d2a48b696d40b3
Nell <nbrisard@coxautoinc.com>
2020-08-27 22:36:07 -0500

    adding a working version

---
+++ Project1/Models/Classes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class Classes
+    {
+        public int ClassId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
