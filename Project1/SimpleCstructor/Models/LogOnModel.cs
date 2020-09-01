a97ec12109950488fbc4c0f8515db0ca5ecd4124
Nell <nbrisard@coxautoinc.com>
2020-09-01 09:40:43 -0500

    Adding missing files.

---
+++ Project1/SimpleCstructor/Models/LogOnModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCstructor.Models
+{
+    public class LogOnModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string UserEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string UserPassword { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
